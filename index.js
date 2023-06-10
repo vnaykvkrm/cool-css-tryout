2d197d10beb45a53791b4e25be36148629cfaadd
vinayak vikram <vnaykvkrm@gmail.com>
2023-06-10 16:41:28 +0530

    edited html and js file

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const track = document.getElementsByClassName('track');
+
 window.onmousedown = e => {
-    e.firstval;
+    track.dataset.mouseDown = e.firstvalue;
+}
+
+window.onmousemove = e => {
+    const mouseDelta= parseFloat(track.dataset.mouseDown) - e.firstvalue,
+        maxDelta=window.innerWidth/2;
+
+    const percentage = (mouseDelta / maxDelta)*-100;
+
+    track.style.transform = `translateX(${percentage}%,-50%)`;
 }
